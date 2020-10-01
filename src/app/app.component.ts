47a369cefd5d41f567592dafd3724b5ab4a3b5f9
mingli <mingli2015md@gmail.com>
2020-10-01 12:14:25 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pps-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ppshop-frontend';
+}
