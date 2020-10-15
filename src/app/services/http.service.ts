b06af7ac00bff08d9c6e53e78c60585afb5eba53
mingli <mingli2015md@gmail.com>
2020-10-15 19:59:22 +0200

    fetch products via http

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,9 +1,9 @@
 import {Injectable} from '@angular/core';
 import {Product} from '../product/model/product.model';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
-import {CategoryType} from '../product/model/category-type.enum';
+import {User} from '../user/model/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +11,26 @@ import {CategoryType} from '../product/model/category-type.enum';
 export class HttpService {
   private BASE_URL = environment.SERVER_URL;
 
-  private products: Product[] = [
-    new Product(1, 'Haribo Goldbären', CategoryType.SNACK, 'Haribo Goldbären', 1.00, 1, '/assets/image/products/haribo1.jpeg'),
-    new Product(2, 'Haribo Tropifrutti', CategoryType.SNACK, 'Haribo Tropifrutti', 1.00, 2, '/assets/image/products/haribo2.jpeg'),
-    new Product(3, 'Haribo Happy-Colo', CategoryType.SNACK, 'Haribo Happy-Colo', 1.00, 3, '/assets/image/products/haribo3.jpeg'),
-  ];
-
   constructor(private httpClient: HttpClient) {
   }
 
-  getAllProducts(numberOfResults = 10): Observable<Product[]> {
+  getAllProducts(): Observable<Product[]> {
     const url = `${this.BASE_URL}/product`;
-    // return this.httpClient.get<Product[]>(url, {params: {limit: numberOfResults.toString()}});
-    return of(this.products);
+    return this.httpClient.get<Product[]>(url);
   }
+
+  login(user: User): Observable<any> {
+    const url = `${this.BASE_URL}/user/login`;
+    const username = user.username;
+    const password = user.password;
+    return this.httpClient.post<any>(url, {username, password});
+  }
+
+  signUp(user: User): Observable<User> {
+    const url = `${this.BASE_URL}/user/signup`;
+    const username = user.username;
+    const password = user.password;
+    return this.httpClient.post<User>(url, {username, password});
+  }
+
 }
